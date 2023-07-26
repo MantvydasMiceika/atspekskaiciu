beb016a95738fea4940b665131acfb7e83e2ed7d
MantvydasMiceika <mantvydas.miceika@gmail.com>
2023-07-26 08:18:26 +0300

    pridetas kodas uzbaigtas atsitiktiniu skaiciu generatorius

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import random
 sugeneruotas = random.randint(1, 101)
 print(sugeneruotas)
 while True:
-    spejimas = int(input("Spekite skaiciu: "))
+    spejimas = int(input("Spekite skaiciu nuo 0 iki 100: "))
     if spejimas > sugeneruotas:
         print("Maziau")
     if spejimas < sugeneruotas:
         print("Daugiau")
-    else:
-        print("Atspejai")
+    if spejimas == sugeneruotas:
+        print(f"Atspejote, Skaičius buvo {sugeneruotas}")
         break
