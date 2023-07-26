39d7ef6d13576ae7be2eb7d161e3581e99b00891
MantvydasMiceika <mantvydas.miceika@gmail.com>
2023-07-26 08:37:41 +0300

    +patobulintas kodas

--- main.py
+++ main.py
@@ -4,15 +4,20 @@ print("Iveskite spejamo skaiciaus diapazona: ")
 nuo = int(input("Nuo: "))
 iki = int(input("Iki: "))
 
+spejimas_nuo = nuo
+spejimas_iki = iki
+
 sugeneruotas = random.randint(nuo, iki)
 spejimai = 0
 print(sugeneruotas)
 while True:
-    spejimas = int(input(f"Spekite skaiciu nuo {nuo} iki {iki}: "))
+    spejimas = int(input(f"Spekite skaiciu nuo {spejimas_nuo} iki {spejimas_iki}: "))
     spejimai += 1
     if spejimas > sugeneruotas:
+        spejimas_iki = spejimas
         print("Maziau")
     if spejimas < sugeneruotas:
+        spejimas_nuo = spejimas
         print("Daugiau")
     if spejimas == sugeneruotas:
         print(f"Atspejote, Skaičius - {sugeneruotas}")
