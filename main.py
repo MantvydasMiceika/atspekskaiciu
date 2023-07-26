b8fe06607b4afedeee31ac5751c6a5dac9942e26
MantvydasMiceika <mantvydas.miceika@gmail.com>
2023-07-26 08:32:17 +0300

    leisti pasirinkti skaiciu diapazona

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import random
 
-sugeneruotas = random.randint(1, 101)
+print("Iveskite spejamo skaiciaus diapazona: ")
+nuo = int(input("Nuo: "))
+iki = int(input("Iki: "))
+
+sugeneruotas = random.randint(nuo, iki)
 spejimai = 0
 print(sugeneruotas)
 while True:
-    spejimas = int(input("Spekite skaiciu nuo 0 iki 100: "))
+    spejimas = int(input(f"Spekite skaiciu nuo {nuo} iki {iki}: "))
     spejimai += 1
     if spejimas > sugeneruotas:
         print("Maziau")
